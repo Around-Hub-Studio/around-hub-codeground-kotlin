abd571df7a499d0c2d2b608c9777a801fe53beca
flature <jungwoo5394@gmail.com>
2023-05-20 01:33:36 +0900

    add additional enum example

--- src/main/kotlin/studio/aroundhub/kotlin/enums/EnumSample.kt
+++ src/main/kotlin/studio/aroundhub/kotlin/enums/EnumSample.kt
@@ -7,14 +7,73 @@ enum class Color(val label: String, val code: String) {
   BLUE("blue", "#0000FF");
 }
 
+enum class HttpStatus(val label: String, val code: Int) {
+  REDIRECT("redirect", 300) {
+    override fun isServerError(): Boolean {
+      return false
+    }
+  },
+  BAD_REQUEST("bad request", 400) {
+    override fun isServerError(): Boolean {
+      return false
+    }
+  },
+  INTERNAL_ERROR("internal error", 500) {
+    override fun isServerError(): Boolean {
+      return true
+    }
+  };
+
+  abstract fun isServerError(): Boolean
+}
+
+enum class HttpStatus2(val label: String, val code: Int) : CheckStatus {
+  REDIRECT("redirect", 300) {
+    override fun isServerError(): Boolean {
+      return false
+    }
+  },
+  BAD_REQUEST("bad request", 400) {
+    override fun isServerError(): Boolean {
+      return false
+    }
+  },
+  INTERNAL_ERROR("internal error", 500) {
+    override fun isServerError(): Boolean {
+      return true
+    }
+  };
+}
+
+interface CheckStatus {
+  fun isServerError(): Boolean
+}
+
 fun main() {
   val red = Color.RED
   println(red)
   println(red.label)
   println(red.code)
 
+  println()
+
   println("[[ color code list ]]")
-  for(temp in Color.values()){
+  for (temp in Color.values()) {
     println("${temp.label} is ${temp.code}")
   }
+
+  println()
+
+  println(HttpStatus.BAD_REQUEST)
+  println(HttpStatus.BAD_REQUEST.label)
+  println(HttpStatus.BAD_REQUEST.code)
+
+  println()
+
+  println(HttpStatus.BAD_REQUEST.isServerError())
+  println(HttpStatus.INTERNAL_ERROR.isServerError())
+
+  println(HttpStatus2.BAD_REQUEST.isServerError())
+  println(HttpStatus2.INTERNAL_ERROR.isServerError())
+
 }
